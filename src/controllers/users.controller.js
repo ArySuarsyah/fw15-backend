1b541008f370a96d48ed7050bdbe081fa4a56894
arsrsyh <gotrany97@gmail.com>
2023-04-10 10:40:25 +0800

    error handler

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,5 @@
-
 const userModels = require("../models/users.model");
-const errorHandler = require("../helpers/error.handler");
+const errorHandler = require("../helpers/errorHandler");
 
 exports.createUser = async (req, res) => {
   if (!req.body.password.lenght || !req.body.email.lenght) {
@@ -32,48 +31,60 @@ exports.getAllUsers = async (req, res) => {
 };
 
 exports.getUserById = async (req, res) => {
-  const data = await userModels.getUserById(req.params.id);
-  console.log(data);
-  if (data) {
-    return res.status(200).json({
-      success: true,
-      message: "Access success",
-      data: data,
-    });
-  } else {
-    return res.status(404).json({
-      success: false,
-      message: "User not found!",
-    });
+  try {
+    const data = await userModels.getUserById(req.params.id);
+    console.log(data);
+    if (data) {
+      return res.status(200).json({
+        success: true,
+        message: "Access success",
+        data: data,
+      });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: "User not found!",
+      });
+    }
+  } catch (err) {
+    return errorHandler(err, res);
   }
 };
 
 exports.updateUsers = async (req, res) => {
-  const data = await userModels.update(req.body, req.params.id);
-  if (data) {
-    return res.json({
-      success: true,
-      message: "User updated!",
-      results: data,
+  try {
+    const data = await userModels.update(req.body, req.params.id);
+    if (data) {
+      return res.json({
+        success: true,
+        message: "User updated!",
+        results: data,
+      });
+    }
+    return res.status(404).json({
+      success: false,
+      message: "User not found!",
     });
+  } catch (err) {
+    return errorHandler(err, res);
   }
-  return res.status(404).json({
-    success: false,
-    message: "User not found!",
-  });
 };
 
 exports.deleteUsers = async (req, res) => {
-  const data = await userModels.deleteUsers(req.params.id);
-  if (data) {
-    return res.json({
-      success: true,
-      message: "Users deleted successfully",
-      results: data,
+  try {
+    const data = await userModels.deleteUsers(req.params.id);
+    if (data) {
+      return res.json({
+        success: true,
+        message: "Users deleted successfully",
+        results: data,
+      });
+    }
+    return res.status(404).json({
+      success: false,
+      message: "User not found!",
     });
+  } catch (err) {
+    return errorHandler(err, res);
   }
-  return res.status(404).json({
-    success: false,
-    message: "User not found!",
-  });
 };
