116b6ad8a0cea4814d5abb1011e5ad034528a8ac
arsrsyh <gotrany97@gmail.com>
2023-07-09 21:31:50 +0800

    filter

--- src/helpers/filter.helper.js
+++ src/helpers/filter.helper.js
@@ -1,12 +1,13 @@
 
 const filterData = (data) => {
+  console.log(data.limit);
+console.log((parseInt(data.page) - 1) * parseInt(data.limit));
   return {
-    limit: data.limit || 5,
-    page: (parseInt(data.page) - 1) * parseInt(data.limit)||0,
+    limit: parseInt(data.limit || 5),
+    page: (parseInt(data.page) - 0) * parseInt(data.limit),
     search: data.search || "",
     sort: data.sort || "id",
     sortBy: data.sortBy || "ASC",
   };
 }
-
 module.exports = filterData
