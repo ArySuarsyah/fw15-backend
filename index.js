abd328a0539f31fafa8cb1554b25268c618454ea
arsrsyh <gotrany97@gmail.com>
2023-07-09 21:28:25 +0800

    fixed index

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const cors = require("cors");
 const app = express();
 
 
-
 app.use(cors());
+
 app.use(express.urlencoded({ extended: false }));
 app.use("/", require("./src/routes"));
 
@@ -18,3 +18,34 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`App running on ${PORT}`);
 });
+
+
+// require("dotenv").config({
+//   path: ".env",
+// });
+// /* eslint-disable no-undef */
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+// const http = require("http").createServer(app);
+// const io = require("socket.io")(http);
+
+// app.use(cors());
+// app.use(express.urlencoded({ extended: false }));
+// app.use("/", require("./src/routes"));
+
+// const PORT = process.env.PORT;
+
+
+// io.on("connection", (socket) => {
+//   console.log("Klien terhubung");
+
+//   socket.on("pesan", (data) => {
+//     console.log("Pesan diterima:", data);
+//   });
+
+// });
+
+// http.listen(PORT, () => {
+//   console.log(`App running on ${PORT}`);
+// });
