c577c8b8d27fba75e0a06b17eae505e40ac102c2
arsrsyh <gotrany97@gmail.com>
2023-07-10 12:04:31 +0800

    remove console.log

--- src/helpers/filter.helper.js
+++ src/helpers/filter.helper.js
@@ -1,7 +1,5 @@
 
 const filterData = (data) => {
-  console.log(data.limit);
-console.log((parseInt(data.page) - 1) * parseInt(data.limit));
   return {
     limit: parseInt(data.limit || 5),
     page: (parseInt(data.page) - 0) * parseInt(data.limit),
