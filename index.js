9f1398d0b1891f31dddce15eb306fccc64b38e33
arsrsyh <gotrany97@gmail.com>
2023-07-21 13:55:32 +0800

    cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 
 const corsOptions = {
-  origin: "https://localhost:5173",
+  origin: "http://localhost:5173",
 };
 
 app.use(cors(corsOptions));
