90f7d7bdb8567ee4e505bda0bf6bd3ca17419787
arsrsyh <gotrany97@gmail.com>
2023-08-08 13:13:17 +0800

    edit profile

--- src/controller/profileController.js
+++ src/controller/profileController.js
@@ -101,6 +101,7 @@ exports.deleteProfile = async (req, res) => {
 };
 
 exports.updateProfileByUserId = async (req, res) => {
+  console.log(req)
   try {
     const { id } = req.user;
     const user = await profileModels.getProfileByUserId(id);
